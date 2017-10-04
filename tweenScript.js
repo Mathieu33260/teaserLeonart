793beb67902f7f550d60989689747bbe11978848
Ngras <nicolas.g33@hotmail.fr>
2017-10-04 15:36:10 +0200

    enhance css selector

--- tweenScript.js
+++ tweenScript.js
@@ -3,12 +3,14 @@ var nom = document.getElementById('pictureNameAnimation');
 var logo = document.getElementById('pictureLogoAnimation');
 var tl = new TimelineLite();
 
-tl.add(TweenLite.to(nom, 1.3, {width:400, height:300}));
-tl.add(TweenLite.to(logo, 1.3, {width:200, height:170}));
+tl.add(TweenLite.to(nom, 1.5, {width:400, height:300}));
+tl.add(TweenLite.to(logo, 1.5, {width:200, height:170}));
 tl.add(TweenLite.to(nom, 1.8, {opacity:0}));
-TweenLite.to(logo, 1.8, {opacity:0, delay:2.6});
+TweenLite.to(logo, 1.8, {opacity:0, delay:3});
 tl.call(displayContainer);
 
 function displayContainer() {
-  var container = document.getElementById('containerSection');
+  document.getElementById('wallpaperSection').removeChild(document.getElementById('animationSection'));
+
+  delete containerDelete;
 }
