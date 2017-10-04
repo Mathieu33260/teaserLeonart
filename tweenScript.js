eaefe3cb354375f429c47f322557f969c7fca6ed
Ngras <nicolas.g33@hotmail.fr>
2017-10-04 15:18:29 +0200

    opening animation

--- tweenScript.js
+++ tweenScript.js
@@ -1,5 +1,14 @@
-var obj = {myProp:0};
-TweenLite.to(obj, 2, {myProp:100});
 
-var carre = document.getElementById('carre');
-TweenLite.to(carre, 1.5, {width:100, height:200});
+var nom = document.getElementById('pictureNameAnimation');
+var logo = document.getElementById('pictureLogoAnimation');
+var tl = new TimelineLite();
+
+tl.add(TweenLite.to(nom, 1.3, {width:400, height:300}));
+tl.add(TweenLite.to(logo, 1.3, {width:200, height:170}));
+tl.add(TweenLite.to(nom, 1.8, {opacity:0}));
+TweenLite.to(logo, 1.8, {opacity:0, delay:2.6});
+tl.call(displayContainer);
+
+function displayContainer() {
+  var container = document.getElementById('containerSection');
+}
