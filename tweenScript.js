f0c07e840a636edcecc025f447e37cbc3732a811
Florent Michenaud <florentm@MacBook-Pro-de-Florent.local>
2017-10-05 11:58:14 +0200

    deleting brush.svg

--- tweenScript.js
+++ tweenScript.js
@@ -1,25 +1,21 @@
 
-var namePicture = document.getElementById('pictureNameAnimation');
-var logo = document.getElementById('pictureLogoAnimation');
-var title = document.getElementById('pictureTitle');
+var container = document.getElementById('containerBlock');
+var slogan = document.getElementById('slogan')
+var section = document.getElementById('animationSection');
+var tl = new TimelineLite();
 
-window.onload = function opening() {
-  var tl = new TimelineLite();
+tl.from(container, 1, {
+	scale: 0
+})
+.to(container, 1,{
+	top: "-40%"
+});
+tl.from(slogan, 1, {
+	left: -500,
+	opacity: 0
+});
 
-  tl.add(TweenLite.from(namePicture, 1.5, {autoAlpha:0, scale:0.5}));
-  TweenLite.from(logo, 1.5, {autoAlpha:0, scale:0.5}, 0);
-  tl.add(TweenLite.to(namePicture, 1.8, {opacity:0}));
-  TweenLite.to(logo, 1.8, {opacity:0, delay:1.5});
-  tl.call(deleteAnimation);
-
-  displayContainer();
-}
-
-function displayContainer() {
-  var tlContainer = new TimelineLite();
-}
-
-function deleteAnimation() {
-  document.getElementById('wallpaperSection').removeChild(document.getElementById('animationSection'));
-  delete containerDelete;
-}
+// In case we want to remove our block
+/*tl.to(container, 1, {
+	opacity: 0
+})*/
