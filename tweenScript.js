8805a79b8d3bca63667457d16c38fb9cdf8b6acf
fmichenaud <florentm@MacBook-Pro-de-Florent.local>
2017-10-05 14:06:15 +0200

    modification animations

--- tweenScript.js
+++ tweenScript.js
@@ -2,18 +2,24 @@
 var container = document.getElementById('containerBlock');
 var slogan = document.getElementById('slogan')
 var section = document.getElementById('animationSection');
+var logoAnimation = document.getElementById('logoAnimation');
 var tl = new TimelineLite();
 
-tl.from(container, 1, {
+tl.from(container, 1.5, {
+	autoAlpha:0,
+	"margin-top" : "10%",
 	scale: 0
 })
 .to(container, 1,{
 	top: "-40%"
-});
-tl.from(slogan, 1, {
-	left: -500,
-	opacity: 0
-});
+}, "az");
+tl.from(slogan, 1, {opacity: 0});
+/*tl.from(logoAnimation, 1, {
+	opacity: 0,
+	"margin-top": "1%"
+}, "az")*/
+
+
 
 // In case we want to remove our block
 /*tl.to(container, 1, {
